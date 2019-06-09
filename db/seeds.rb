1ec0483c9750f5ef8114465b36146f4f9abae661
solidstatejake <jberg3@binghamton.edu>
2019-06-09 18:48:51 -0400

    Created seeds file for sample data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+	Blog.create!(
+			title: "My Blog Post #{blog + 1}",
+			body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+Accusantium cum distinctio dolore, dolores earum fuga labore magni natus, optio
+praesentium ratione rem velit! Ad amet at atque autem, beatae commodi dicta
+distinctio ea eos ex impedit ipsam ipsum labore nam officiis pariatur quasi,
+quia quis quos similique ut velit vero voluptatum! Accusamus adipisci aliquam
+asperiores aspernatur cupiditate dolore, est et eum excepturi facere fugiat
+incidunt libero maxime mollitia neque nobis officia pariatur possimus
+praesentium quasi quia quis reiciendis sint, soluta unde vel voluptatem
+voluptatum! A, asperiores aspernatur debitis, dolor ex fugit illo in ipsam,
+nisi numquam quidem reiciendis sunt veritatis."
+	)
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+	Skill.create!(
+			 title: "Rails #{skill + 1}",
+			 percent_utilized: 5 * (skill + 1)
+	)
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+	Portfolio.create!(
+					 title: "Portfolio title: #{portfolio_item + 1}",
+					 subtitle: "Service #{portfolio_item + 1}",
+					 body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+ Debitis dicta hic iure perspiciatis possimus repellat sed. A accusantium,
+ consectetur consequuntur cumque cupiditate deleniti deserunt dolorum eaque eos
+ esse est et ex fugiat illo magnam, molestias, nam neque omnis pariatur quas qui
+ saepe sed sit soluta tempora tenetur totam voluptatum. Asperiores?",
+					 main_image: "https://placehold.it/600x400",
+					 thumb_image: "https://placehold.it/350x200"
+	)
+end
+
+puts "9 portfolio items created"
