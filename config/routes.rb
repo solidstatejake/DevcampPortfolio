45d90f0a15879e49df08aac8c5e4552cea51800b
solidstatejake <jberg3@binghamton.edu>
2019-06-10 13:59:37 -0400

    Configured custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
+# Here in the routes file, we can pass whatever relative path we want to the
+# first argument of get. That way, we can make the url whatever we want.
+# The second argument has to be an `Action#Controller` pair.
+#
+# SEE `rake routes` in the terminal to determine where to route.
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	root to: 'pages#home'
 end
 
