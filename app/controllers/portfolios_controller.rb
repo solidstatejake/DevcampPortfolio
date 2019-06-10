f80716decea84c00d9881d379b2d672faa6c06a9
solidstatejake <jberg3@binghamton.edu>
2019-06-09 22:47:34 -0400

    Implemented show functionality for portfolio module

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,42 +1,52 @@
+# frozen_string_literal: true
+
 class PortfoliosController < ApplicationController
+  def index
+    @portfolio_items = Portfolio.all
+  end
 
-	def index
-		@portfolio_items = Portfolio.all
-	end
+  def new
+    @portfolio_item = Portfolio.new
+  end
 
-	def new
-		@portfolio_item = Portfolio.new
-	end
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html do
+          redirect_to portfolios_path,
+                      notice: 'Your portfolio item is now live.'
+        end
+      else
+        format.html { render :new }
+      end
+    end
+  end
 
-	def create
-		@portfolio_item = Portfolio.new(portfolio_params)
-		respond_to do |format|
-			if @portfolio_item.save
-				format.html { redirect_to portfolios_path,
-				              notice: 'Your portfolio item is now live.' }
-			else
-				format.html { render :new }
-			end
-		end
-	end
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 
-	def edit
-		@portfolio_item = Portfolio.find(params[:id])
-	end
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html do
+          redirect_to portfolios_path,
+                      notice: 'The record was successfully updated.'
+        end
+      else
+        format.html { render :edit }
+      end
+    end
+  end
 
-	def update
-		@portfolio_item = Portfolio.find(params[:id])
-		respond_to do |format|
-			if @portfolio_item.update(portfolio_params)
-				format.html { redirect_to portfolios_path,
-				              notice: 'The record was successfully updated.' }
-			else
-				format.html { render :edit }
-			end
-		end
-	end
+  def show
+	  @portfolio_item = Portfolio.find(params[:id])
 
-	def portfolio_params
-		params.require(:portfolio).permit(:title, :subtitle, :body)
-	end
+  end
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
 end
