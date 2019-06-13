edfb00088559e45eac699045f09a6eae30ccaac2
solidstatejake <jberg3@binghamton.edu>
2019-06-12 20:30:40 -0400

    Completed authentication with virtual attributes for user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
   # LOOK THESE UP TO UNDERSTAND
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_presence_of :name
+
+  def first_name
+    self.name.split.first
+  end
+
+  def last_name
+    self.name.split.last
+  end
+
 end
