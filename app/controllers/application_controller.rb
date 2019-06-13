a437ca1fbc1cf5945a1ea33133e285a276f7b3af
solidstatejake <jberg3@binghamton.edu>
2019-06-13 14:36:46 -0400

    Added guest user for current_user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include SetSource
+  include CurrentUserConcern
 end
