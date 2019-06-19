0420b4e1ab1ecad30cf25e65e59fa78f557c8817
solidstatejake <jberg3@binghamton.edu>
2019-06-19 12:16:17 -0400

    Updated portfolio to include attribute for position.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,7 +8,7 @@ class PortfoliosController < ApplicationController
          site_admin: :all
 
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.by_position
   end
 
   def new
