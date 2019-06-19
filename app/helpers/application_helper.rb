ea73c00e8a34fd863afa9a6dc0a55c134d84ca36
solidstatejake <jberg3@binghamton.edu>
2019-06-19 09:40:29 -0400

    Implemented refactor for navigation and the active statuses depending on which page the user is on.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,6 +18,45 @@ module ApplicationHelper
   end
 
   def copyright_generator
-    SolidStateViewTool::Renderer.copyright 'Jacob Berg', 'Made with love and Rails'
+    SolidStateViewTool::Renderer.copyright 'Jacob Berg', 'Made with Rails, Bootstrap, and love'
+  end
+
+  def nav_items
+    [
+    {
+    url:   root_path,
+    title: 'Home'
+    },
+    {
+    url:   about_me_path,
+    title: 'About'
+    },
+    {
+    url:   contact_path,
+    title: 'Contact'
+    },
+    {
+    url:   blogs_path,
+    title: 'Blog'
+    },
+    {
+    url:   portfolios_path,
+    title: 'Portfolio'
+    }
+
+    ]
+  end
+
+  def nav_helper tag_type, style
+    nav_links = '';
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{is_active? item[:url]}'>#{item[:title]}</#{tag_type}>"
+
+    end
+    nav_links.html_safe
+  end
+
+  def is_active? path
+    'active' if current_page? path
   end
 end
