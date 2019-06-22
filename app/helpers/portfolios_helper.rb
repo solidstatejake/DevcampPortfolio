8a0ec1c4a70c52c2e713fdddae850c47981386e8
solidstatejake <jberg3@binghamton.edu>
2019-06-22 14:12:55 -0400

    Refactored portfolios image helper method.

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -4,14 +4,12 @@ module PortfoliosHelper
     "http://placehold.it/#{height}x#{width}"
   end
 
-  def portfolio_img img, type
-    if img.model.main_image? || img.model.thumb_image?
-      img.to_s
-    elsif type == 'thumb'
-      image_generator(height: '350', width: '200')
+  def portfolio_img portfolio, type
+    if type == 'thumb'
+      portfolio.thumb_image.url || image_generator(height: '350', width: '200')
     elsif type == 'main'
-      image_generator(height: '600', width: '400')
+      portfolio.main_image.url || image_generator(height: '600', width: '400')
     end
-
   end
+
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
 
   config.aws_attributes = -> { {
-  expires: 1.week.from_now.httpdate,
+  expires:       1.week.from_now.httpdate,
   cache_control: 'max-age=604800'
   } }
 
@@ -14,7 +14,7 @@ CarrierWave.configure do |config|
   access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
   secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
   region:            ENV.fetch('AWS_REGION'), # Required
-  stub_responses:    Rails.env.test? # Optional, avoid hitting S3 actual during tests
+  stub_responses: Rails.env.test? # Optional, avoid hitting S3 actual during tests
   }
 
 end
